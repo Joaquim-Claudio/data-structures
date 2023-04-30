6c68163bd9c98799ac49f06f38fd522f97f8cb80
joaquim <joaquim.claudio2003@gmail.com>
2023-04-30 01:42:31 +0100

    WIP

---
+++ hash-table/src/hash-table.c
@@ -0,0 +1,78 @@
+#include "hash-table.h"
+
+typedef struct{
+    void* key;
+    void* value;
+    bool (*key_equal)(void*, void*);
+    void (*key_destroy)(void*);
+}t_Item, *Item;
+
+
+struct t_HashTable{
+    int size;
+    int num_elements;
+    List* table;
+    bool (*key_equal)(void*, void*);
+    void (*key_destroy)(void*);
+    int (*hash)(void*, int);
+};
+
+// Creates a new hash table.
+HashTable hash_table_create(int size, int (*hash)(void*, int), bool (*key_equal)(void*, void*), void(*key_destroy)(void*)){
+    return NULL;
+}
+
+// Destroys a hash table.
+void hash_table_destroy(HashTable table, void (*value_destroy)(void*)){
+    return NULL;
+}
+
+// Inserts a new key-value pair into the hash table.
+void hash_table_insert(HashTable table, void* key, void* value){
+    return NULL;
+}
+
+// Removes a key-value pair from the hash table.
+void* hash_table_remove(HashTable table, void* key){
+    return NULL;
+}
+
+// Returns the value associated with the key.
+void* hash_table_get(HashTable table, void* key){
+    return NULL;
+}
+
+// Updates the value associated with the key.
+void* hash_table_update(HashTable table, void* key, void* value){
+    return NULL;
+}
+
+// Returns the number of elements in the hash table.
+int hash_table_size(HashTable table){
+    return NULL;
+}
+
+// Returns true iff the hash table contains no elements.
+bool hash_table_is_empty(HashTable table){
+    return NULL;
+}
+
+// Returns true iff the hash table contains no elements.
+List hash_table_keys(HashTable table){
+    return NULL;
+}
+
+// Returns the keys of the hash table.
+List hash_table_values(HashTable table){
+    return NULL;
+}
+
+// Returns the values of the hash table.
+List hash_table_entries(HashTable table){
+    return NULL;
+}
+
+// Returns the entries of the hash table.
+void hash_table_rehash(HashTable table, int new_size){
+    return NULL;
+}
---
+++ hash-table/src/hash-table.h
@@ -0,0 +1,48 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <stdbool.h>
+#include "../utils/list.h"
+
+
+#define DEFAULT_SIZE 127
+
+typedef struct t_HashTable* HashTable;
+
+// Creates a new hash table.
+HashTable hash_table_create(int size, int (*hash)(void*, int), bool (*key_equal)(void*, void*), void(*key_destroy)(void*));
+
+// Destroys a hash table.
+void hash_table_destroy(HashTable table, void (*value_destroy)(void*));
+
+// Inserts a new key-value pair into the hash table.
+void hash_table_insert(HashTable table, void* key, void* value);
+
+// Removes a key-value pair from the hash table.
+void* hash_table_remove(HashTable table, void* key);
+
+// Returns the value associated with the key.
+void* hash_table_get(HashTable table, void* key);
+
+// Updates the value associated with the key.
+void* hash_table_update(HashTable table, void* key, void* value);
+
+// Returns the number of elements in the hash table.
+int hash_table_size(HashTable table);
+
+// Returns true iff the hash table contains no elements.
+bool hash_table_is_empty(HashTable table);
+
+// Returns true iff the hash table contains no elements.
+List hash_table_keys(HashTable table);
+
+// Returns the keys of the hash table.
+List hash_table_values(HashTable table);
+
+// Returns the values of the hash table.
+List hash_table_entries(HashTable table);
+
+// Returns the entries of the hash table.
+void hash_table_rehash(HashTable table, int new_size);
+
+#endif
